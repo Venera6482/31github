3db6c38d0648ae9650246e98391f31593dc5376b
Venera <venerakockonova181@gmail.com>
2023-11-07 00:11:12 +0600

    1 task

---
+++ EdabitConverter.java
@@ -0,0 +1,14 @@
+public class EdabitConverter {
+    public int intoSeconds(int minutes){
+        int result = minutes*60;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        EdabitConverter min = new EdabitConverter();
+        int minutes = 5;
+        int convertToSeconds = min.intoSeconds(minutes);
+        System.out.printf(minutes+" minutes is equal to "+convertToSeconds+" seconds");
+    }
+
+}
