3c833e26110cbfab64131f376c833c82c3f712b9
Venera <venerakockonova181@gmail.com>
2023-11-10 23:32:17 +0600

    6 task

---
+++ Upvotes.java
@@ -0,0 +1,10 @@
+public class Upvotes {
+        public static int getVoteCount(int upvotes,int downvotes) {
+            return upvotes-downvotes;
+        }
+
+    public static void main(String[] args) {
+        getVoteCount(12, -45);
+
+    }
+}
