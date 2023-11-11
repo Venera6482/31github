009a016196500c929f15db2291ed7252468e212c
Venera <venerakockonova181@gmail.com>
2023-11-11 20:06:12 +0600

    7 task

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static int power(int x, int y){
+        int sum = x*y;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(5,8524));
+    }
+}
