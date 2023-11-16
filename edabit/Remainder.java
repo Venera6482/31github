516e9d23150218a8ea6c699876a1a61c477c0412
Venera <venerakockonova181@gmail.com>
2023-11-16 22:31:27 +0600

    12 task

---
+++ edabit/Remainder.java
@@ -0,0 +1,11 @@
+package edabit;
+
+public class Remainder {
+    public static int operator(int a, int b){
+        return a%b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(operator(5,8));
+    }
+}
