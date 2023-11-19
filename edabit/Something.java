10574714661d5d869b1dc9f5ce65d3839f136d3d
Venera <venerakockonova181@gmail.com>
2023-11-19 15:22:59 +0600

    15 task

---
+++ edabit/Something.java
@@ -0,0 +1,11 @@
+package edabit;
+
+public class Something {
+    public static String returnSomething(String text){
+        return "Something "+text;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(returnSomething("I love in this life"));
+    }
+}
