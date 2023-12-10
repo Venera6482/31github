42c84d980ecc338b9d06033c8c9e1f23672bcd0c
Venera <venerakockonova181@gmail.com>
2023-12-10 09:15:46 +0600

    35 task

---
+++ codinbat/StartHi.java
@@ -0,0 +1,13 @@
+package codinbat;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class StartHi {
+    public static String hi(String s){
+        return "Hi "+s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Growth hungry");
+    }
+}
