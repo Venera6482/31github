17d9bffb0ea640d3e862b77a05cf35e39f4e632b
Venera <venerakockonova181@gmail.com>
2023-12-11 22:46:26 +0600

    36 task

--- codinbat/IcyHot.java
+++ codinbat/IcyHot.java
@@ -1,17 +1,18 @@
 package codinbat;
 
 public class IcyHot {
-    public static boolean find(double temp1, double temp2){
+    public static boolean find(double temp1, double temp2) {
         // Given two temperatures, return true if one is less than 0 and the other is greater than 100
-        if (temp1<0||temp2>100){
+        if (temp1 < 0 || temp2 > 100) {
             return true;
-        }if (temp1>100||temp2<0){
+        }
+        if (temp1 > 100 || temp2 < 0) {
             return true;
-        }else return false;
+        } else return false;
     }
 
     public static void main(String[] args) {
         System.out.println(find(-10, 200));
-        System.out.println(find(2,9));
+        System.out.println(find(2, 9));
     }
 }
